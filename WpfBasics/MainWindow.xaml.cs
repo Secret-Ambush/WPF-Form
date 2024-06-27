6fd76e58dea1c2702e08ecaa1ee490e7e4a0b90d
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-06-27 13:54:17 +0400

    Update MainWindow.xaml.cs

--- WpfBasics/MainWindow.xaml.cs
+++ WpfBasics/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +19,40 @@ namespace WpfBasics
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckBox.IsChecked = false;
+            this.AssemblyCheckBox.IsChecked = false;
+            this.PlasmaCheckBox.IsChecked = false;
+            this.LaserCheckBox.IsChecked = false;
+            this.PurchaseCheckBox.IsChecked = false;
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += ((string)((CheckBox)sender).Content + " ");
+        }
+
+        private void Purchase_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.Note == null) { return; }
+            
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            this.Note.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Purchase_SelectionChanged(this.Purchase, null);
         }
     }
 }
